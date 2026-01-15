c244c44dddfdf686aace0e9b8998a12a55871960
Abdoul Aziz Zongo <abdoul.aziz.zongo@gmail.com>
2026-01-15 18:27:35 +0100

    Day 1 complete: EKS cluster provisioned and access validated

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,66 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.project_name}-${var.environment}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnets = ["10.0.10.0/24", "10.0.11.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = local.tags
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "${var.project_name}-${var.environment}"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+  
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    default = {
+      name = "ng-default"
+
+      instance_types = ["t3.micro"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      subnet_ids = module.vpc.private_subnets
+
+      tags = local.tags
+    }
+  }
+
+  tags = local.tags
+}
+
---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-3"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = "devsecops-lab"
+}
+
+variable "project_name" {
+  type    = string
+  default = "cn-devsecops"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
